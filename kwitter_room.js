a52b7ea3126e708e46d0ba5a73325cf2ab73c0f1
Tib-pixel <80082315+Tib-pixel@users.noreply.github.com>
2021-07-26 12:31:45 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDQDwUgvZP657XO9PyaTLWKHI18u6XI-Ow",
+    authDomain: "school-chat-46996.firebaseapp.com",
+    projectId: "school-chat-46996",
+    storageBucket: "school-chat-46996.appspot.com",
+    messagingSenderId: "490637409644",
+    appId: "1:490637409644:web:03a04e1aa389fd7d44c749"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
